3fc8932fd94eb61130d310a5d1c581b3947078e7
lukejordan <luke.jordan@marks-and-spencer.com>
2015-06-20 17:50:09 +0100

    More changes

--- script/alias.sh
+++ script/alias.sh
@@ -1,5 +1,16 @@
 #! 
 echo "Loading aliases..."
+export SCRIPT_HOME=~/Development/devessentials/script
+echo Set script home as: ${SCRIPT_HOME}
+alias scripthome="cd ${SCRIPT_HOME}"
+alias showhiddenfiles="defaults write com.apple.finder AppleShowAllFiles YES"
+alias hidehiddenfiles="defaults write com.apple.finder AppleShowAllFiles NO"
+
+source ${SCRIPT_HOME}/config.sh
 alias bashreload="source ~/.profile"
-alias cheese="echo MOOO" 
+alias cheese="echo MOOO"
+alias aem6vanillaauthhome=""
+alias aem6vanillapubhome="cd ${AEM_VANILLA_HOME}/publish"
+alias aem6authhome=""
+alias aem6pubhome=""
 echo "Aliases loaded"
---
+++ script/config.sh
@@ -0,0 +1,4 @@
+#!
+export AEM_VANILLA_HOME=~/Development/Training/aem
+export AEM_HOME=/
+echo ${AEM_VANILLA_HOME}
