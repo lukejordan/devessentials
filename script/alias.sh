81328455d17875ca062ff766cc0f3471327c1ccd
lukejordan <luke.jordan@marks-and-spencer.com>
2016-02-08 14:41:25 +0000

    Latest alias and foods deploy job

--- script/alias.sh
+++ script/alias.sh
@@ -11,7 +11,14 @@ source ${SCRIPT_HOME}/script_mac.sh
 
 # Goto
 alias devhome="cd ${DEVELOPMENT_HOME}"
+alias foodshome="cd ${FOODS_HOME}"
 alias gotodevops="cd ${DEVELOPMENT_HOME}/devenv-devops"
+alias tomcathome="cd ${TOMCAT_HOME}"
+alias apachehome="cd /usr/local/etc/apache2/2.2"
+
+alias deployrda="open -a Terminal.app ${SCRIPT_HOME}/deployFoods.sh"
+alias deployrdabe="open -a Terminal.app ${SCRIPT_HOME}/deployFoodsBe.sh"
+
 
 # Misc
 alias bashreload="source ~/.profile"
@@ -20,6 +27,9 @@ alias aliasedit="atom ${SCRIPT_HOME}/alias.sh"
 
 alias editpom="atom pom.xml"
 
+alias sshrdadev="ssh ${CLOUD_USER}@${DEV_RDA_CLOUD_HOST}"
+alias sshrda="ssh ${CLOUD_USER}@${OLD_DEV_RDA_CLOUD_HOST}"
+alias sshrdasit="ssh ${CLOUD_USER}@${SIT_RDA_CLOUD_HOST}"
 alias sshcloudwcsauth="ssh ${CLOUD_USER}@${WCS_AUTH_CLOUD_HOST}"
 alias sshcloudwcspub="ssh ${CLOUD_USER}@${WCS_PUB_CLOUD_HOST}"
 alias sshcloudaem="ssh ${CLOUD_USER}@${AEM_CLOUD_HOST}"
---
+++ script/deployFoods.sh
@@ -0,0 +1,24 @@
+#!
+export TOMCAT_HOME=~/install/apache-tomcat-8.0.28
+export APP_PATH=~/Development/code
+export APP_NAME=foods-psa-rda-webapp
+export WAR_NAME=rda
+
+echo "Stop tomcat"
+${TOMCAT_HOME}/bin/shutdown.sh
+
+echo "Build Application"
+#cd ~/Development/sideproject/spring-webapp/app-parent
+cd ${APP_PATH}/${APP_NAME}
+mvn clean package -Dmaven.test.skip -Pfull-build -Drun.test.fe=build-local
+
+echo "Clean webapp from tomcat webapps"
+rm -rf ${TOMCAT_HOME}/webapps/${WAR_NAME}*
+echo "Clean tomcat logs"
+rm -rf ${TOMCAT_HOME}/logs/*
+
+echo "Deploy webapp"
+cp target/${WAR_NAME}*.war ${TOMCAT_HOME}/webapps/${WAR_NAME}.war
+
+echo "Start tomcat"
+${TOMCAT_HOME}/bin/startup.sh
