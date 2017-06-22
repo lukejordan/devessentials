20babd9a4e5b9fb30877c84390f56e16640c3d05
lukeyj <lukeyj@gmail.com>
2017-06-22 15:02:53 +0100

    Setting up laptop for work

--- script/personal_linux.sh
+++ script/personal_linux.sh
@@ -5,6 +5,7 @@ export DEV_ESSENTIALS_HOME=/opt/development/devessentials
 #echo Load Private Config from ${DEV_ESSENTIALS_HOME}
 source ${DEV_ESSENTIALS_HOME}/config/private_config.sh
 source ${DEV_ESSENTIALS_HOME}/script/alias.sh
+source ${DEV_ESSENTIALS_HOME}/script/tmg/work_alias.sh
 #source ${DEV_ESSENTIALS_HOME}/script/alias_work.sh
 
 source ${DEV_ESSENTIALS_HOME}/script/home_linux/env_config.sh
